81881d9a5db4637b87d42af64afced6aa541c36b
Farid Bass <abdoulfaridbassirou7898@gmail.com>
2024-10-10 21:28:37 +0300

    creating the book model

--- littleLemon_API/views.py
+++ littleLemon_API/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+from .models import Book
+from django.views.decorators.csrf import csrf_exempt
+from django.forms.models import model_to_dict
 
-# Create your views here.
