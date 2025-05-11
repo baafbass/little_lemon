37c0b5a188b03ea8863f069b15540191213f4338
Farid Bass <abdoulfaridbassirou7898@gmail.com>
2025-05-11 15:01:46 +0300

    little limon req

--- meta_caps/meta_frt/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Footer = () =>{
-    return (
-        <Footer></Footer>
-    )
-}
-
-export default Footer;
--- meta_caps/meta_frt/src/components/Main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Main = () =>{
-    return (
-        <main></main>
-    )
-}
-
-export default Main;
